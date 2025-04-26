e0b8c501b2705834630d634e0edfa7dd6f9fe087
jimatas <jim.atas@greenwheels.nl>
2025-04-26 12:46:29 +0200

    Add tests for generic FromType overload.

--- test/Zift.Tests/ParameterNameGeneratorTests.cs
+++ test/Zift.Tests/ParameterNameGeneratorTests.cs
@@ -28,6 +28,42 @@ public class ParameterNameGeneratorTests
         Assert.Equal("x", result);
     }
 
+    [Theory]
+    [InlineData(typeof(Category), "c")]
+    [InlineData(typeof(Product), "p")]
+    [InlineData(typeof(Review), "r")]
+    [InlineData(typeof(Review[]), "r")]
+    [InlineData(typeof(IList<Product>), "i")]
+    [InlineData(typeof(_123Type), "t")]
+    public void FromTypeGeneric_TypeNameStartsWithAsciiLetter_ReturnsLowercaseLetter(Type type, string expectedName)
+    {
+        var fromTypeGenericMethod = typeof(ParameterNameGenerator)
+            .GetMethod(nameof(ParameterNameGenerator.FromType),
+                genericParameterCount: 1,
+                Type.EmptyTypes)!
+            .MakeGenericMethod(type);
+
+        var result = (string?)fromTypeGenericMethod.Invoke(null, null);
+
+        Assert.Equal(expectedName, result);
+    }
+
+    [Theory]
+    [InlineData(typeof(_123))]
+    [InlineData(typeof(Çĺâşş))]
+    public void FromTypeGeneric_TypeNameWithoutAsciiLetters_ReturnsDefaultName(Type type)
+    {
+        var fromTypeGenericMethod = typeof(ParameterNameGenerator)
+            .GetMethod(nameof(ParameterNameGenerator.FromType),
+                genericParameterCount: 1,
+                Type.EmptyTypes)!
+            .MakeGenericMethod(type);
+
+        var result = (string?)fromTypeGenericMethod.Invoke(null, null);
+
+        Assert.Equal("x", result);
+    }
+
     #region Fixture
     private class _123Type;
     private class _123;
